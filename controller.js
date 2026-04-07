c1c95f6cda2c15fbd32528d6433cf63d99ec32a5
jee014 <akkiakki7972@gmail.com>
2026-04-07 17:43:52 +0530

    Add files via upload

---
+++ controller.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+
+    // display message from python
+    eel.expose(DisplayMessage)
+    function DisplayMessage(message) {
+        // Replace the hidden textillate <li>
+        $(".siri-message .texts li").text(message);
+
+
+    }
+
+    // Display hood
+    eel.expose(ShowHood)
+    function ShowHood() {
+        $("#Oval").attr("hidden", false);
+        $("#SiriWave").attr("hidden", true);
+    }
+
+
+    eel.expose(senderText)
+    function senderText(message) {
+        var chatBox = document.getElementById("chat-canvas-body");
+        if (message.trim() !== "") {
+            chatBox.innerHTML += `<div class="row justify-content-end mb-4">
+            <div class = "width-size">
+            <div class="sender_message">${message}</div>
+        </div>`; 
+    
+            // Scroll bottom
+            chatBox.scrollTop = chatBox.scrollHeight;
+        }
+    }
+
+    eel.expose(receiverText)
+    function receiverText(message) {
+
+        var chatBox = document.getElementById("chat-canvas-body");
+        if (message.trim() !== "") {
+            chatBox.innerHTML += `<div class="row justify-content-start mb-4">
+            <div class = "width-size">
+            <div class="receiver_message">${message}</div>
+            </div>
+        </div>`; 
+    
+            // Scroll to the bottom of the chat box
+            chatBox.scrollTop = chatBox.scrollHeight;
+        }
+        
+    }
+
+    
+
+});
+
